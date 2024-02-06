7e541a90c9f930befb69560277a3ab30bff3f9ba
matildemaribo <126452785+matildemaribo@users.noreply.github.com>
2024-02-06 23:23:15 +0100

    Opstart
    Opstart på hjem siden med et billede fra computeren

---
+++ script.js
@@ -0,0 +1,78 @@
+function show() {
+    document.getElementById('sidebar').classList.toggle('active');
+  }
+  
+  
+  document.addEventListener('scroll', function () {
+    var nav = document.getElementById('upper-nav');
+    var visibleSection = null;
+  
+    for (var i = 1; i <= 10; i++) {
+      var sectionId = 'section' + i;
+      var section = document.getElementById(sectionId);
+  
+      if (section) {
+        var rect = section.getBoundingClientRect();
+  
+        if (rect.top <= window.innerHeight && rect.bottom >= window.innerHeight) {
+          visibleSection = section;
+  
+          // Reset the color of all pic elements
+          for (var j = 1; j <= 10; j++) {
+            var allpicElements = document.querySelectorAll('#pic' + j);
+            allpicElements.forEach(function (picElement) {
+              picElement.style.color = ''; // Reset color to default
+            });
+          }
+  
+          // Set the color of current section's pic elements
+          var picElements = document.querySelectorAll('#pic' + i);
+          picElements.forEach(function (picElement) {
+            picElement.style.color = '#D49824';
+          });
+  
+          break; // Exit the loop if a visible section is found
+        }
+      }
+    }
+  
+    if (visibleSection && window.scrollY > 600) {
+      nav.style.display = 'block';
+    } else {
+      nav.style.display = 'none';
+    }
+  });
+  
+  
+  const isElementInViewport = (el) => {
+    const rect = el.getBoundingClientRect();
+    return (
+      rect.top >= 0 &&
+      rect.left >= 0 &&
+      rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+      rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+    );
+  };
+  
+  const handleFadeIn = () => {
+    const isMobile = window.innerWidth <= 768; // Adjust the width as needed for your definition of mobile devices
+    const fadeElements = document.querySelectorAll('.fade-in');
+    
+    fadeElements.forEach((element) => {
+      if (!isMobile) {
+        if (isElementInViewport(element)) {
+          element.classList.add('active');
+        }
+      } else {
+        element.classList.add('active'); // Show elements instantly on mobile
+      }
+    });
+  };
+  
+  document.addEventListener('DOMContentLoaded', () => {
+    handleFadeIn();
+  });
+  
+  window.addEventListener('scroll', () => {
+    handleFadeIn();
+  });
